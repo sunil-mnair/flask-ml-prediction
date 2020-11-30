404b2ef296f3647c743ec1c5c50858611a7c29cf
sunil-mnair <sunil.mnair@outlook.com>
2020-11-30 11:28:38 +0400

    Sample ML App

--- forms.py
+++ forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,BooleanField
-from wtforms.validators import InputRequired
+from wtforms import StringField,PasswordField,BooleanField,DateField,SelectField,SubmitField,RadioField
+from wtforms.validators import InputRequired,DataRequired
+from flask_wtf.file import FileField,FileRequired,FileAllowed
+# import phonenumbers
 
 username_required = "Please provide a username"
 password_required = "Please provide a password"
@@ -13,3 +15,40 @@ class LoginForm(FlaskForm):
 class SignupForm(FlaskForm):
     username = StringField('Username',validators=[InputRequired(message=username_required)])
     password = PasswordField('Password',validators=[InputRequired(message=password_required)])
+
+class BuildModelForm(FlaskForm):
+    xtrain = FileField('XTrain', validators=[FileRequired(), FileAllowed(['csv'], 'CSV Files only!')])
+    ytrain = FileField('YTrain', validators=[FileRequired(), FileAllowed(['csv'], 'CSV Files only!')])
+
+class UploadForm(FlaskForm):
+    # pickle = FileField('Pickle', validators=[FileRequired(), FileAllowed(['pkl'], 'Pickle Files only!')])
+    new_data = FileField('Data', validators=[FileRequired(), FileAllowed(['csv'], 'CSV Files only!')])
+
+# class EnquiryForm(FlaskForm):
+#     enquiry_dt = DateField(validators=[InputRequired()])
+#     name = StringField('Username',validators=[InputRequired(message=username_required)])
+#     gender = RadioField(choices=[('Male','Male'),('Female','Female')])
+#     birth_dt = DateField(validators=[InputRequired()])
+#     school = SelectField()
+#     fathers_name = StringField('Fathers Name')
+#     fathers_phone = IntegerField('Fathers Phone Number')
+#     fathers_email =
+#     mothers_name = 
+#     mothers_phone = 
+#     mothers_email =
+
+# class PhoneForm(FlaskForm):
+#     enquiry_dt = DateField(validators=[InputRequired()])
+#     name = StringField('Username',validators=[InputRequired(message=username_required)])
+#     gender = RadioField(choices=[('Male','Male'),('Female','Female')])
+#     birth_dt = DateField(validators=[InputRequired()])
+#     fathers_phone = StringField('Fathers Phone', validators=[DataRequired()])
+#     mothers_phone = StringField('Mothers Phone', validators=[DataRequired()])
+
+#     def validate_phone(self, phone):
+#         try:
+#             p = phonenumbers.parse(phone.data)
+#             if not phonenumbers.is_valid_number(p):
+#                 raise ValueError()
+#         except (phonenumbers.phonenumberutil.NumberParseException, ValueError):
+#             raise ValidationError('Invalid phone number')
--- models.py
+++ models.py
@@ -7,10 +7,151 @@ from flask_login import UserMixin,current_user,login_user,logout_user
 from flask_admin import Admin,AdminIndexView
 from flask_admin.contrib.sqla import ModelView
 
+from sqlalchemy.sql import expression
+
 class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
     username = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
+
+
+class Enquiry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    enquiry_dt = db.Column(db.DateTime, nullable = False)
+
+    name = db.Column(db.String(100))
+    gender = db.Column(db.String(4))
+    birth_dt = db.Column(db.DateTime,nullable = True)
+
+    country = db.Column(db.String(100))
+    school_id = db.Column(db.Integer,db.ForeignKey('school.id'))
+    parent_id = db.Column(db.Integer,db.ForeignKey('parent.id'))
+
+    email = db.Column(db.String,nullable = True)
+    phone = db.Column(db.String,nullable = True)
+
+    course_id = db.Column(db.Integer,db.ForeignKey('course.id'))
+    source_id = db.Column(db.Integer,db.ForeignKey('source.id'))
+
+    amount = db.Column(db.Float,nullable = False)
+    sessions = db.Column(db.Float,nullable = False)
+
+    remarks = db.Column(db.String(200),nullable = True)
+
+    created_dt = db.Column(db.DateTime, nullable = False,
+    default = datetime.now(uae))
+    modified_dt = db.Column(db.DateTime, nullable = False,
+    default = datetime.now(uae))
+
+class Parent(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    
+    fathers_name = db.Column(db.String(100),nullable = True)
+    fathers_email = db.Column(db.String,nullable = True)
+    fathers_phone = db.Column(db.String,nullable = True)
+
+    mothers_name = db.Column(db.String(100),nullable = True)
+    mothers_email = db.Column(db.String,nullable = True)
+    mothers_phone = db.Column(db.String,nullable = True)
+
+
+    created_dt = db.Column(db.DateTime, nullable = False,
+    default = datetime.now(uae))
+    modified_dt = db.Column(db.DateTime, nullable = False,
+    default = datetime.now(uae))
+
+class School(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    school = db.Column(db.String(200),nullable = False)
+    country = db.Column(db.String())
+    city = db.Column(db.String())
+
+
+class Source(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    source = db.Column(db.String(),nullable = False)
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    category = db.Column(db.String(100),nullable = False)
+    course = db.Column(db.String(100),nullable = False)
+    amount = db.Column(db.Integer,nullable = False)
+    sessions = db.Column(db.Integer,nullable = False)
+
+    # enquiries = db.relationship("EnquiredService",backref='service_master')
+    # sales = db.relationship("SoldService",backref='service_master')
+    # appointment = db.relationship("AppointmentService",backref='service_master')
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    gender = db.Column(db.String(4))
+    birth_dt = db.Column(db.DateTime,nullable = True)
+
+    country = db.Column(db.String(100))
+    school_id = db.Column(db.Integer,db.ForeignKey('school.id'))
+    parent_id = db.Column(db.Integer,db.ForeignKey('parent.id'))
+
+    email = db.Column(db.String,nullable = True)
+    phone = db.Column(db.String,nullable = True)
+
+    source_id = db.Column(db.Integer,db.ForeignKey('source.id'))
+    remarks = db.Column(db.String(200),nullable = True)
+
+    created_dt = db.Column(db.DateTime, nullable = False,
+    default = datetime.now(uae))
+    modified_dt = db.Column(db.DateTime, nullable = False,
+    default = datetime.now(uae))
+
+class Enrollment(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    student_id = db.Column(db.Integer,db.ForeignKey('student.id'),nullable=False)
+    
+    enrollment_date = db.Column(db.DateTime,nullable = False)
+    pipeline_date = db.Column(db.DateTime,nullable = True)
+
+    course_id = db.Column(db.Integer,db.ForeignKey('course.id'))
+    amount = db.Column(db.Float,nullable = False)
+    sessions = db.Column(db.Float,nullable = False)
+
+    start_date = db.Column(db.DateTime,nullable = True)
+    paid = db.Column(db.Boolean, server_default=expression.false(),default=False,nullable=False)
+
+    installment = db.Column(db.Boolean, server_default=expression.false(),default=False,nullable=False)
+    
+    created_dt = db.Column(db.DateTime, nullable = False,
+    default = datetime.now(uae))
+    modified_dt = db.Column(db.DateTime, nullable = False,
+    default = datetime.now(uae))
+
+class Renewal(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    student_id = db.Column(db.Integer,db.ForeignKey('student.id'),nullable=False)
+    
+    course_id = db.Column(db.Integer,db.ForeignKey('course.id'))
+    amount = db.Column(db.Float,nullable = False)
+    sessions = db.Column(db.Float,nullable = False)
+
+    created_dt = db.Column(db.DateTime, nullable = False,
+    default = datetime.now(uae))
+    modified_dt = db.Column(db.DateTime, nullable = False,
+    default = datetime.now(uae))
+
+class Installment(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    enrollment_id = db.Column(db.Integer,db.ForeignKey('enrollment.id'),nullable=False)
+
+    payment_date = db.Column(db.DateTime,nullable = False)
+    amount = db.Column(db.Float,nullable = False)
+
+    payment_type = db.Column(db.String,nullable = False)
+
+    created_dt = db.Column(db.DateTime, nullable = False,
+    default = datetime.now(uae))
+    modified_dt = db.Column(db.DateTime, nullable = False,
+    default = datetime.now(uae))
+ 
 
 
 
